86209850c91d9f5a600ff418587861181dc1619c
Srinjoy <srinjy.s.01.in@gmail.com>
2020-11-14 17:39:56 +0530

    Test

---
+++ TestClass.java
@@ -0,0 +1,10 @@
+public class TestClass {
+    @Override
+    public String toString() {
+        return "Srinjoy";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new TestClass().toString());
+    }
+}
